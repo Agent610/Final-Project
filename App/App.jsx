5440eb3d997451524d2474bd34b08d196e42944d
Agent610 <BhavsarParthD96@Gmail.com>
2025-04-27 15:43:57 -0400

    Correct file structure

---
+++ App/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import LoginModal from "../LoginModal/LoginModal";
+import Main from "../Main/Main";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import Navigation from "../Navigation/Navigation";
+import NewsCard from "../NewsCard/NewsCard";
+import Preloader from "../Preloader/Preloader";
+import RegisterModal from "../RegisterModal/RegisterModal";
+import SearchForm from "../final-project/src/components/SearchForm/SearchForm";
+
+function App() {
+  return <></>;
+}
+
+export default App;
