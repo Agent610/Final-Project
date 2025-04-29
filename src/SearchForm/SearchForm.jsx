da42c9dcc3ac674c6c4775a18a123bf8fb5328fe
Agent610 <BhavsarParthD96@Gmail.com>
2025-04-29 19:09:47 -0400

    Searchform started

--- src/SearchForm/SearchForm.jsx
+++ src/SearchForm/SearchForm.jsx
@@ -1,1 +1,27 @@
-//export default SearchForm;
+import { useState } from "react";
+import "./SearchForm.css";
+
+function SearchForm() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <form className="search-form">
+      <input
+        type="text"
+        placeholder="Enter topic"
+        className="search-input"
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <button type="submit" placeholder="search-button">
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchForm;
