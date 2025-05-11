d791d68d0d087099f270d736ea1ba69693310704
Agent610 <BhavsarParthD96@Gmail.com>
2025-05-11 18:33:39 -0400

    Footer is done

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,28 +1,41 @@
 import React from "react";
 import "./footer.css";
 import { Link } from "react-router-dom";
+import github from "../../images/github.png";
+import socialmedia from "../../images/social-media.png";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__content">
+        <nav className="footer__nav">
+          <Link to="/" className="footer__nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="footer__nav-link">
+            TripleTen
+          </Link>
+        </nav>
+        <div className="footer__icons">
+          <a
+            href="https://github.com/Agent610"
+            className="footer__nav-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="Github" className="footer__icon" />
+          </a>
+          <a href="#" className="footer__nav-link">
+            <img
+              src={socialmedia}
+              alt="Social Media"
+              className="footer__icon"
+            />
+          </a>
+        </div>
         <p className="footer__copyright">
           © 2024 Supersite Powered by News API
         </p>
-        <nav className="footer__nav">
-          <ul className="footer__nav-list">
-            <li className="footer__nav-item">
-              <Link to="/" className="footer__nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="footer__nav-item">
-              <Link to="/about" className="footer__nav-link">
-                About
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     </footer>
   );
