d7b197c8afdfe6c39501951e0e6dcbcc3fe40e37
Agent610 <BhavsarParthD96@Gmail.com>
2025-05-12 18:56:35 -0400

    Only Hamburger menu left in Navigation + Header worked

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,28 +1,10 @@
 import { useContext } from "react";
 import "./Header.css";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
-import Navigation from "../Navigation/Navigation";
-import { Link } from "react-router-dom";
 
-function Header({ isLoggedIn, handleSigninClick }) {
-  const currentUser = useContext(CurrentUserContext);
-
+function Header() {
   return (
     <header className="header">
       <h1>News Explorer</h1>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <Navigation isLoggedIn={!!currentUser} />
-      {!currentUser && (
-        <button
-          onClick={handleSigninClick}
-          type="button"
-          className="header__signIn"
-        >
-          Sign In
-        </button>
-      )}
     </header>
   );
 }
--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -2,19 +2,79 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 
-function Navigation({ isLoggedIn }) {
+function Navigation({
+  isLoggedIn,
+  currentUser,
+  handleSigninClick,
+  handleSignoutClick,
+}) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navigation">
-      <ul className="nav-list">
-        {isLoggedIn && (
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        {isMenuOpen ? (
+          /*Close (X) icon */
+          <div className="close-icon">
+            <span className="close-icon__line"></span>
+            <span className="close-icon__line"></span>{" "}
+          </div>
+        ) : (
+          /*Hamburger menu lines */
           <>
-            <li>
-              <Link to="/saved-news">Saved Articles</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
+            <span className="hamburger-menu__line"></span>
+            <span className="hamburger-menu__line"></span>
+            <span className="hamburger-menu__line"></span>
           </>
+        )}
+      </button>
+      {isMenuOpen && (
+        <div className="mobile-menu">
+          <ul className="mobile-menu__list">
+            <Link to="/" className="nav-list__link nav-list__link-home">
+              Home
+            </Link>
+          </ul>
+        </div>
+      )}
+      <ul className="nav-list">
+        <li className="nav-list__item">
+          <Link to="/" className="nav-list__link nav-list__link-home">
+            Home
+          </Link>
+        </li>
+
+        {isLoggedIn ? (
+          /* Logged in state */
+          <>
+            <div className="nav-list__auth">
+              <li className="nav-list__item">
+                <Link to="/saved-news" className="nav-list__link">
+                  Saved Articles
+                </Link>
+              </li>
+              <li className="nav-list__item">
+                <button
+                  className="nav-list__button"
+                  onClick={handleSignoutClick}
+                >
+                  {currentUser.name}
+                </button>
+              </li>
+            </div>
+          </>
+        ) : (
+          /* Logged out state */
+          <div className="nav-list__auth">
+            <li className="nav-list__item">
+              <button className="nav-list__button" onClick={handleSigninClick}>
+                Sign in
+              </button>
+            </li>
+          </div>
         )}
       </ul>
     </nav>
