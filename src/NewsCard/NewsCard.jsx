d244843ebbb8ddffde2a59b6527ae747770d073b
Agent610 <BhavsarParthD96@Gmail.com>
2025-06-26 18:04:46 -0400

    Save article button is done 100%

--- src/NewsCard/NewsCard.jsx
+++ src/NewsCard/NewsCard.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./NewsCard.css";
 import { useLocation } from "react-router-dom";
+import SaveMarked from "../../images/save-marked.svg";
+import SaveunMarked from "../../images/save-unmarked.svg";
 
 function NewsCard({
   title,
@@ -18,11 +20,13 @@ function NewsCard({
   const [isHovered, setIsHovered] = useState(false);
   const location = useLocation();
   const isHome = location.pathname === "/";
+  const [wasJustSaved, setWasJustSaved] = useState(false);
 
   const handleClick = () => {
     if (isHome) {
       if (isLoggedIn) {
         onSaveArticle(title, description, source, link, image);
+        setWasJustSaved(true);
       }
     } else if (onDeleteArticle) {
       onDeleteArticle(link);
@@ -33,7 +37,30 @@ function NewsCard({
     <article className="news-card">
       <div className="news-card__image-container">
         <img src={image} alt={title} className="news-card__image" />
+
+        <div
+          className="news-card__save-wrapper"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <button
+            className={`news-card__save-button ${
+              isSaved ? "news-card__save-button_saved" : ""
+            }`}
+            onClick={handleClick}
+            aria-label={isSaved ? "Remove from saved" : "Save article"}
+            style={{
+              backgroundImage: `url(${
+                isSaved || wasJustSaved ? SaveMarked : SaveunMarked
+              })`,
+            }}
+          />
+          {!isLoggedIn && isHovered && (
+            <div className="news-card__tooltip">Sign in to save article</div>
+          )}
+        </div>
       </div>
+
       <div className="news-card__content">
         <p className="news-card__date">{date}</p>
         <h3 className="news-card__title">{title}</h3>
@@ -48,25 +75,6 @@ function NewsCard({
           >
             Show more
           </a>
-
-          <div
-            className="news-card__save-wrapper"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            <button
-              className={`news-card__save-button ${
-                isSaved ? "news-card__save-button_saved" : ""
-              }`}
-              onClick={handleClick}
-            >
-              {isSaved ? "Remove from saved" : "Save article"}
-            </button>
-
-            {!isLoggedIn && isHovered && (
-              <div className="news-card__tooltip">Sign in to save article</div>
-            )}
-          </div>
         </div>
       </div>
     </article>
